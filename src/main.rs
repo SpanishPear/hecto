fa47bc71882beb2b7bc010325a8fff006d342ab9
SpanishPear <shrey.somaiya@gmail.com>
2021-07-31 01:35:47 +1000

    beginning of ch2

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{self, stdout};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
+fn die(e: std::io::Error) {
+    std::panic::panic_any(e);
+}
+
+fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                }
+
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
+
+            Err(err) => die(err),
+        }
+    }
+}
