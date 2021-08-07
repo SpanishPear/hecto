0559e07586fb7cd0e02dc763e6e103d6a86596b4
SpanishPear <shrey.somaiya@gmail.com>
2021-08-07 13:37:05 +1000

    refactor into functions n stuff

--- src/editor.rs
+++ src/editor.rs
@@ -12,24 +12,22 @@ impl Editor {
     pub fn run(&self) {
         
         let _stdout = stdout().into_raw_mode().unwrap();
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if let Key::Ctrl(c) = key {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    } 
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
-                },
-
-                Err(err) => die(err),
+       
+        loop {
+            if let Err(error) = self.process_keypresses() {
+                die(error);
             }
         }
+    }
 
+    fn process_keypresses(&self) -> Result<(), std::io::Error> {
+        
+        let pressed_key = read_key()?;
+        match pressed_key  {
+            Key::Ctrl('q') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
     }
 
     // this is essentially an init function 
@@ -39,6 +37,16 @@ impl Editor {
         Self {}
     }
 }
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+        
+    }
+}
+
 
 fn die(e: std::io::Error) {
    std::panic::panic_any(e);
